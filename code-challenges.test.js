6799e988bf7158060e80338acf908fcd5e265224
ArtyG-10 <a.gourent@outlook.com>
2021-07-16 22:17:04 -0400

    finished

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -18,48 +18,110 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var num1 = 15
-// Expected output: "15 is divisible by three"
-
-var num2 = 0
-// Expected output: "0 is divisible by three"
-
-var num3 = -7
-// Expected output: "-7 is not divisible by three"
-
-
-
+// var num1 = 15
+// // Expected output: "15 is divisible by three"
+//
+// var num2 = 0
+// // Expected output: "0 is divisible by three"
+//
+// var num3 = -7
+// // Expected output: "-7 is not divisible by three"
 // b) Create the function that makes the test pass.
 
 
+//I start my test with describe() and then the function name as a string
+describe("divByThree",()=>{
+//Declare my test variables
+  let num1 = 15
+  let num2 = 0
+  let num3 = -7
+//next use it() and describe what the function is supposed to do
+  it("tells you if a number is divisable by three",()=>{
+//Write my expect().toEqual() statements, one for every test variable
+    expect(divByThree(num1)).toEqual(`${num1} is divisible by three`)
+    expect(divByThree(num2)).toEqual(`${num2} is divisible by three`)
+    expect(divByThree(num3)).toEqual(`${num3} is not divisible by three`)
+  })
+})
+//Fist I create a function called divByThree which takes a number as an argument
+const divByThree = (number) => {
+//I start with an if statement which uses modulo of 3 to see if it is evenly divisible.
+  if (number%3===0){
+//If it checks correct I use a string interpolation to return a statement that the argument was divisible by 3
+    return `${number} is divisible by three`
+  }
+//In every other case it would return a string interpolation that it was not divisible.
+  else return `${number} is not divisible by three`
+}
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-
-var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
-
-
-
+// var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+// // // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+// //
+// var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
+// // // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
 // b) Create the function that makes the test pass.
 
 
+
+//Start my test with describe() and then the function name as a string
+describe("capitalizeArray",()=>{
+//Declare my test variables
+  let randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+  let randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
+//next use it() and describe what the function is supposed to do
+  it("takes in array of words and returns array with all words capitalized",()=>{
+//Write my expect().toEqual() statements, one for each test variable
+    expect(capitalizeArray(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+    expect(capitalizeArray(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango", "Deduction"])
+  })
+})
+//declare a function called capitalizeArray that takes in an array
+const capitalizeArray = (array) => {
+//.map the array and .toUpperCase the [0] index of each value
+  return array.map(value => {
+//Had to google how to add the rest of the string back to the uppercased first letter
+    return value[0].toUpperCase() + value.substring(1)})
+}
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var vowelTester1 = "learn"
-// Expected output: 1
-var vowelTester2 = "academy"
-// Expected output: 0
-var vowelTester3 = "challenge"
-// Expected output: 2
-
-
-
+// var vowelTester1 = "learn"
+// // // Expected output: 1
+// var vowelTester2 = "academy"
+// // // Expected output: 0
+// var vowelTester3 = "challenge"
+// // // Expected output: 2
 // b) Create the function that makes the test pass.
+
+
+//Start my test with describe() and then the function name as a string
+describe("indexOfFirstVowel",()=>{
+//declare my test variables
+  let vowelTester1 = "learn"
+  let vowelTester2 = "academy"
+  let vowelTester3 = "challenge"
+//next use it() and describe what the function is supposed to do
+  it("takes in a string and logs the index of the first vowel", () => {
+//Write my expect().toEqual() statements, one for each test variable
+    expect(indexOfFirstVowel(vowelTester1)).toEqual(1)
+    expect(indexOfFirstVowel(vowelTester2)).toEqual(0)
+    expect(indexOfFirstVowel(vowelTester3)).toEqual(2)
+  })
+})
+//Fist I create a function called indexOfFirstVowel which takes a string as an argument
+const indexOfFirstVowel = (string) => {
+//I need to declare a variable for a vowels list that I will use to check the string with
+  var vowelList = "aeiouAEIOU"
+//I need to iterate through my string. I can use a simple for loop.
+  for (let i=0; i<string.length; i++){
+//I need to set up a conditional that iterates until it finds a character in the string which can be found in my vowel list at which point i can just return the current i value.
+    if (vowelList.indexOf(string[i]) !== -1){
+      return i}
+    }
+}
--- interview-questions.md
+++ interview-questions.md
@@ -9,42 +9,41 @@ Then, research the question to expand on your answer. Even if you feel you have 
 1. What is iteration?
 
   Your answer:
-
+    Iteration is the process of running the same code multiple times. Higher order functions like .map and .filter use iteration by running the same code on each value of an array. Each time the code is run it is another iteration.
   Researched answer:
-
+    Iteration is when a program repeats a task for however many times you specify until a condition is met. A loop can theoretically be iterated forever, at which point you can press ctrl+C.
 
 
 2. The built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
   Your answer:
-
+    the .map method takes in (value, index, array) in that order. The value parameter is the only required parameter and the rest are situational.
   Researched answer:
-
+    The .map method takes the predefined parameters of value, index, and array. The value and index are at current iteration and the array is the array that was passed in. They have to be in that order but can be named something arbitrary. The value parameter is required and the rest are optional.
 
 
 3. What is object destructuring?
 
   Your answer:
-
+    I am not 100% sure on how object destructuring works but I know it is used to break up the data contained in objects.
   Researched answer:
-
+    Object destruction is a JavaScript expression which makes it possible to unpack data from objects into distinct variables. The syntax is a variable declaration followed by curly brackets and a list of variables and after the second curly bracket it is set equal to the object name. The variables in the curly brackets are named after the corresponding keys in the object and assigned the designated value.
 
 
 4. What is the difference between an object and a class?
 
   Your answer:
-
+    An object is similar to an array in that it is a thing which holds multiple pieces of data, each piece of data is identified by its key. A class is just a template from which you can create a new object which inherits the data in the class. Classes can be used to manipulate and create modified objects as you see fit.
   Researched answer:
-
+    Objects are collections of values identified by paired keys. A class is a blueprint for an object but it is not an object until it is created and assigned to a variable using the protected language 'new'. Classes can be modified and customized.
 
 
 5. Why would you use the method super()?
 
   Your answer:
-
+    The method super() would be used when you are extending a class into a subclass. You need it to pass parameters into the parent class. If you don't need to pass any parameters you would still include super but with empty parenthesis.
   Researched answer:
-
-
+    Super() is contained within the constructor() method and used in an extended class which is inheriting information from the parent class. The super arguments pass into the arguments of the constructor in the parent class.
 
 6. STRETCH: What is hoisting in JavaScript?
 
@@ -59,11 +58,15 @@ Then, research the question to expand on your answer. Even if you feel you have 
 Research and define the following terms to the best of your ability.
 
 1. React:
-
+    React is a JavaScript library used to create user interfaces or UIs. These can be webpages or mobile apps. Since it is based on encapsulated components it is very scaleable and can be used to put together very complex UIs.
 2. React lifecycle methods:
+    The three phases of the React lifecycle are Mounting, Updating, and Unmounting. First a component is created and mounted into the DOM. Then any changed to the state are updated. lastly the component is removed from the DOM.
 
 3. React state:
+    State is an object managed within a component that stores dynamic data and determines how the component renders and behaves.
 
 4. Component invocation/call:
-
+    Nesting components allows us to call a component within the return of another component.
+    
 5. DOM Events:
+    Document Object Model (DOM) events occur when a user interacts with the application. Events normally execute functions when the event occurs (such as when a user clicks a button).
